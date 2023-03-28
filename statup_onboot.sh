a3963cea209c7992b3b92589a6c014fa9842fa46
Alex Redko <alexcopy@mail.ru>
2023-03-28 12:35:21 +0100

    update startup script

--- statup_onboot.sh
+++ statup_onboot.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-SERVICE= "^python\d?\s+main\.py"
-path_to_service= "/"
-if pgrep -f "$SERVICE" > /dev/null
+
+if pgrep -f "python main\.py" > /dev/null
 then
     echo "Solar Pond App is running"
 else
-    echo "$SERVICE stopped"
-    exec python  "$path_to_service"
+    echo "The Service is stopped"
+    cd /path/to/python/script/ || exit
+    python  main.py
+    # Start screen session 1 and run custom script
+    screen -S info -d -m bash -c 'tail --follow=name logs/main_inf.log'
+
+    # Start screen session 2 and run custom script
+    screen -S debug -d -m bash -c 'tail --follow=name logs/info.log'
+
+    # Start screen session 3 and run custom script
+    screen -S error -d -m bash -c 'tail --follow=name logs/error.log'
+    echo "Three detached screen sessions created and custom scripts started."
 fi
