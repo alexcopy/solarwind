09abbd53273f8fb3a21b0f703ccfb9066cf43e53
Alex Redko <alexcopy@mail.ru>
2023-03-30 16:33:43 +0100

    refactor logs

--- malina/LIB/PrintLogs.py
+++ malina/LIB/PrintLogs.py
@@ -90,10 +90,10 @@ class SolarLogging:
 
         wtg = (solar_current['10m_solar_current'] * fifo_buffer['1s_inverter_bus_voltage']) / 1000
         self.logging.info(" Inverter Status is: %s  " % 'ON' if (inverter_status == 1) else "OFF")
-        self.logging.info(" Main Relay works from: %s  " % "MAIN" if (statuses['main_relay_status'] == 0) else "OFF")
+        self.logging.info(" Main Relay works from: %s  " % "INVERT" if (statuses['main_relay_status'] == 0) else "MAIN")
         self.logging.info("")
 
-        self.logging.info(" UV Sterilizer is: %s " % "ON" if (load_devices.get_uv_sw_state['switch_1']) else "INVERT")
+        self.logging.info(" UV Sterilizer is: %s " % "ON" if (load_devices.get_uv_sw_state['switch_1']) else "OFF")
         self.logging.info(" FNT Pump State is: %s " % "ON" if (load_devices.get_fnt_sw_state['switch_1']) else "OFF")
         self.logging.info("")
 
