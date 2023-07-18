60897c03ecc7bf18a9c3ef2f06b0c8752325f355
Alex Redko <alexcopy@mail.ru>
2023-07-18 13:10:00 +0100

    fix logger config

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import logging.handlers
 import os
 import time
 from pathlib import Path
+
 last_run = 0
 
 
@@ -17,17 +18,24 @@ class SetupLogger():
 
     def conf_logger(self):
         current_path = Path('logs')
-        log_name = time.strftime("debug")
+        current_path.mkdir(parents=True, exist_ok=True)  # Создаем папку logs, если она не существует
+        log_name = time.strftime("%Y-%m-%d_%H-%M-%S")
         filename = current_path.joinpath(f'{log_name}.log')
+
+        # Создаем обработчик для основного лога
         log_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=3000000, backupCount=5)
-        formatter = logging.Formatter(
-            '%(asctime)s program_name [%(process)d]: %(message)s',
-            '%b %d %H:%M:%S')
-        formatter.converter = time.gmtime  # if you want UTC time
+        formatter = logging.Formatter('%(asctime)s program_name [%(process)d]: %(message)s', '%b %d %H:%M:%S')
+        formatter.converter = time.gmtime  # Если вы хотите использовать время в формате UTC
         log_handler.setFormatter(formatter)
+
+        # Создаем объект логгера и устанавливаем уровень логирования
         logger = logging.getLogger()
-        logger.addHandler(log_handler)
         logger.setLevel(logging.DEBUG)
+
+        # Добавляем обработчик к логгеру
+        logger.addHandler(log_handler)
+
+        # Создаем обработчики для разных уровней логирования
         error_handler = self.setup_logger(current_path, formatter, 'error.log', logging.ERROR)
         logger.addHandler(error_handler)
         warn_handler = self.setup_logger(current_path, formatter, 'warning.log', logging.WARNING)
@@ -37,12 +45,12 @@ class SetupLogger():
         debug_handler = self.setup_logger(current_path, formatter, 'debug.log', logging.DEBUG)
         logger.addHandler(debug_handler)
 
-    def setup_logger(self, current_path, formatter, log_file, level):
-        log = logging.handlers.RotatingFileHandler(os.path.join(current_path, log_file), maxBytes=3000000,
-                                                   backupCount=5)
-        log.setFormatter(formatter)
-        log.setLevel(level)
-        return log
+    @staticmethod
+    def setup_logger(current_path, formatter, filename, level):
+        handler = logging.handlers.RotatingFileHandler(current_path.joinpath(filename), maxBytes=3000000, backupCount=5)
+        handler.setFormatter(formatter)
+        handler.setLevel(level)
+        return handler
 
 
 def _stats():
@@ -52,13 +60,12 @@ def _stats():
     # Проверяем, находимся ли мы в диапазоне с 19:00 до 6:00
     if hour >= 19 or hour < 6:
         if (timestamp - last_run) >= 600:
-            logging.error(f"Update Stats every 600 sec {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+            logging.debug(f"Update Stats every 600 sec {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
             sp.update_devs_stats()
             last_run = timestamp
     else:
-        logging.error(f"Update Stats every 120 sec: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        logging.debug(f"Update Stats every 120 sec: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
         sp.update_devs_stats()
-
 
 
 if __name__ == '__main__':
