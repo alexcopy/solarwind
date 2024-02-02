798e6a0bb969cec538eaa1f18c97180cccb593cf
Alex Redko <alexcopy@mail.ru>
2024-02-02 15:01:46 +0000

    clean up old unused libraries, fix on-boot bash to fix duplicated screens

--- statup_onboot.sh
+++ statup_onboot.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-cd /path/to/python/script/ || exit
 SESSION_NAMES=("info" "debug" "error" "warning")
-if pgrep -f "python main\.py" >/dev/null; then
-  echo "Solar Pond App is running"
-else
-  echo "The Service is stopped"
-  nohup python main.py >logs/run.log &
-  # Start screen session 1 and run custom script
-  screen -S info -d -m bash -c 'tail --follow=name logs/main_inf.log'
 
-  # Start screen session 2 and run custom script
-  screen -S debug -d -m bash -c 'tail --follow=name logs/info.log'
+start_screen_session() {
+    screen -S "$1" -d -m bash -c "tail --follow=name logs/$1.log"
+    echo "Detached screen session created: $1."
+}
 
-  # Start screen session 3 and run custom script
-  screen -S error -d -m bash -c 'tail --follow=name logs/error.log'
-  echo "Three detached screen sessions created and custom scripts started."
+start_python_script() {
+    if pgrep -f "python main\.py" >/dev/null; then
+        echo "Solar Pond App is running"
+    else
+        echo "The Service is stopped"
+        nohup python main.py >logs/run.log &
+    fi
+}
 
-  # Start screen session 3 and run custom script
-  screen -S warning -d -m bash -c 'tail --follow=name logs/warning.log'
-  echo "Three detached screen sessions created and custom scripts started."
-fi
+# Check if Python script is running
+start_python_script
 
-# Loop through the session names and check if each one is still running
+# Check if existing screen sessions are running with the same names
 for SESSION_NAME in "${SESSION_NAMES[@]}"; do
-  if ! screen -list | grep -q "$SESSION_NAME"; then
-    # If the screen session has been terminated, restart it
-    screen -S "$SESSION_NAME" -d -m bash -c "tail --follow=name logs/$SESSION_NAME.log"
-    echo "Detached screen sessions created: $SESSION_NAME."
-  fi
+    if ! screen -list | grep -q "$SESSION_NAME"; then
+        start_screen_session "$SESSION_NAME"
+    else
+        echo "Screen session already running: $SESSION_NAME."
+    fi
 done
