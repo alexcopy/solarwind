ce04c6589aee3af811659e836ffd27803908f558
Alex Redko <alexcopy@mail.ru>
2024-02-05 23:33:50 +0000

    add temp to log print

--- malina/tests/SonOffSwitchTestCase.py
+++ malina/tests/SonOffSwitchTestCase.py
@@ -6,53 +6,46 @@ import hashlib
 import hmac
 import base64
 import json
+import logging
 from unittest.mock import Mock
+from tuya_iot import (
+    TuyaOpenAPI,
+    AuthType,
+    TuyaOpenMQ,
+    TuyaDeviceManager,
+    TUYA_LOGGER, TuyaCloudOpenAPIEndpoint
+)
+# from tuya_connector import (
+# 	TuyaOpenAPI,
+# 	TuyaOpenPulsar,
+# 	TuyaCloudPulsarTopic,
+# )
 
 import requests
 import python_weather
 import asyncio
 import os
 
+sys.path.append('../')
+
 from malina.LIB.InitiateDevices import InitiateDevices
 from malina.LIB.TuyaController import TuyaController
 
-sys.path.append('../')
 from malina.LIB.TuyaAuthorisation import TuyaAuthorisation
-
 
 from dotenv import dotenv_values
 
+ACCESS_ID = "vwny7twaucf3qx8ec39n"
+ACCESS_KEY = "7758cc1c3a064802ab3b505870c2c8a2"
+ENDPOINT = TuyaCloudOpenAPIEndpoint.EUROPE
 
 
-mock_time = 60
+
+TUYA_LOGGER.setLevel(logging.DEBUG)
+openapi = TuyaOpenAPI(ENDPOINT, ACCESS_ID, ACCESS_KEY)
+openapi.connect()
+openapi.auth_type = AuthType.SMART_HOME
+deviceManager = TuyaDeviceManager(openapi, TuyaOpenMQ(openapi))
+print(deviceManager.get_device_status("bf8a065d2b72c26f0breco"))
 
 
-class SonOffSwitchTestCase(unittest.TestCase):
-    def test_valid_structure_without_values(self):
-        tuya_auth = TuyaAuthorisation()
-        tuya_controller = TuyaController(tuya_auth)
-        devices = InitiateDevices().device_controller
-
-
-        fontan = devices.get_devices_by_name("inverter")[0] # fontan
-        print("-------------------------STATUS_______________")
-        print(fontan.get_status())
-        tuya_controller.switch_off_device(fontan)
-        time.sleep(2)
-        tuya_controller.update_status(fontan)
-        print("-------------------------STATUS____ON___________")
-        print(fontan.get_status())
-        print("-------------------------STATUS_______________")
-        time.sleep(5)
-        tuya_controller.switch_on_device(fontan)
-        print("-------------------------STATUS OFF_______________")
-        time.sleep(5)
-        tuya_controller.update_status(fontan)
-
-        print("-------------------------STATUS_______________")
-        print(fontan.get_status())
-        print("-----------------Single Status--------STATUS_______________")
-        print(fontan.get_status('status'))
-
-if __name__ == '__main__':
-    unittest.main()
