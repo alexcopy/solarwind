7f8c3cba24f1a3b8addac350488b41e16f16a24f
Alex Redko <alexcopy@mail.ru>
2024-02-05 23:38:56 +0000

    add temp to log print

--- malina/LIB/PrintLogs.py
+++ malina/LIB/PrintLogs.py
@@ -38,6 +38,7 @@ class SolarLogging:
         logging.info("")
 
         devices = load_devices.get_devices_by_device_type("SWITCH")
+        termo = load_devices.get_devices_by_name("watertemp")[0]
         for device in devices:
             logging.info(f"{device.get_desc.ljust(20)}: %s " % (
                 "ON" if (device.get_status('switch_1')) else "OFF"))
@@ -50,6 +51,8 @@ class SolarLogging:
 
         wtg = (sol_current['1s_solar_current'] * self.avg(filo_fifo.filo_buff['1s_inverter_bus_voltage'])) / 1000
         logging.info(f"{'Solar Power'.ljust(20)}: {Fore.GREEN}{wtg:3.2f} W{Style.RESET_ALL}")
+
+        logging.info(f'Pond Water temp is: {Fore.RED}{termo.get_status("temp_current")} {termo.get_status("temp_unit_convert")}{Style.RESET_ALL}')
         logging.info("---")
         logging.info("--------------------------------------------")
         logging.info("--------------------------------------------")
